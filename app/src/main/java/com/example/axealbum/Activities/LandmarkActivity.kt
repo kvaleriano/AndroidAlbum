1ef9373881c173fa83078c8549306086f0c56fe4
kvaleriano <evaleriano97@gmail.com>
2019-06-01 23:04:39 -0500

    Bug fixing, finishing design details and removing may 2019 for now

--- app/src/main/java/com/example/axealbum/Activities/LandmarkActivity.kt
+++ app/src/main/java/com/example/axealbum/Activities/LandmarkActivity.kt
@@ -4,6 +4,7 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.example.axealbum.Adapters.MemoryAdapter
 import com.example.axealbum.Dao.DataBase
 import com.example.axealbum.R
@@ -31,6 +32,20 @@ class LandmarkActivity: AppCompatActivity() {
             val landmark = it.getString("landmark", "Marzo")
             fetchMemories(year, landmark)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val decorView = window.decorView
+
+        val uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        decorView.systemUiVisibility = uiOptions
     }
 
     private fun fetchMemories(year: String, landmark: String) {
--- app/src/main/java/com/example/axealbum/Helper/Extensions.kt
+++ app/src/main/java/com/example/axealbum/Helper/Extensions.kt
@@ -2,21 +2,25 @@ package com.example.axealbum.Helper
 
 import android.graphics.Typeface
 import android.support.annotation.LayoutRes
+import android.support.v4.content.res.ResourcesCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.axealbum.R
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 }
 
 fun TextView.setAppFont() {
-    val typeface = Typeface.createFromAsset(context.assets, "montserrat_light")
+    val typeface = ResourcesCompat.getFont(context, R.font.montserrat_light)
+    //val typeface = Typeface.createFromAsset(context.assets, "montserrat_light")
     setTypeface(typeface)
 }
 
 fun TextView.setAppFontBold() {
-    val typeface = Typeface.createFromAsset(context.assets, "montserrat_bold")
+    val typeface = ResourcesCompat.getFont(context, R.font.montserrat_bold)
+    //val typeface = Typeface.createFromAsset(context.assets, "montserrat_bold")
     setTypeface(typeface)
 }
